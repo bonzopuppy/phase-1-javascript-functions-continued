f3101b650943023cae9d3a24265a9b6bda8232c2
David Baden <davebaden@gmail.com>
2023-11-02 15:11:27 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun());
+console.log(saturdayFun('bobsledding'));
+
+function mondayWork (activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+console.log(mondayWork());
+console.log(mondayWork('play golf'));
+
+function wrapAdjective (flair = "*") {
+    return function selfAffirmation (characteristic = "special"){
+        return `You are ${flair}${characteristic}${flair}!`;
+    }
+}
+
+console.log(wrapAdjective("$$")("money"));
+
+const pumpUp = wrapAdjective("!!!");
+
+console.log(pumpUp("great"));
+
+console.log(wrapAdjective());
+console.log(wrapAdjective("!!"));
